0b69b5c89e9475c46dccfe0b639c6ee220068cbc
Миша <mihail.hramov.1992@gmail.com>
2021-01-09 01:30:39 +0300

    fix injeting

--- app/src/main/java/com/aaa/bbb/ccc/languageteacher/di/module/RepositoryModule.kt
+++ app/src/main/java/com/aaa/bbb/ccc/languageteacher/di/module/RepositoryModule.kt
@@ -1,12 +1,13 @@
 package com.aaa.bbb.ccc.languageteacher.di.module
 
 import com.aaa.bbb.ccc.languageteacher.data.repository.translate.FakeTranslateRepository
+import com.aaa.bbb.ccc.languageteacher.data.repository.translate.ITranslateRepository
 import dagger.Module
 import dagger.Provides
 
 @Module
 class RepositoryModule {
     @Provides
-    fun provideITranslateRepository() = FakeTranslateRepository()
+    fun provideITranslateRepository(): ITranslateRepository = FakeTranslateRepository()
 
 }
