bf9defcb079db5f4ff24066cf8d8b4115fef7406
Миша <mihail.hramov.1992@gmail.com>
2021-01-10 00:09:30 +0300

    create new scope for dagger

---
+++ app/src/main/java/com/aaa/bbb/ccc/languageteacher/di/scope/PhraseTranslatorScope.kt
@@ -0,0 +1,7 @@
+package com.aaa.bbb.ccc.languageteacher.di.scope
+
+import javax.inject.Scope
+
+@Scope
+@kotlin.annotation.Retention(AnnotationRetention.RUNTIME)
+annotation class PhraseTranslatorScope
