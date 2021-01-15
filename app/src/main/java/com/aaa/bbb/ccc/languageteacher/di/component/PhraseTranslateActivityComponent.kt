deb1fa721842f8f48b5425f7cac80f6c31178922
Миша <mihail.hramov.1992@gmail.com>
2021-01-16 01:11:16 +0300

    change inject method

--- app/src/main/java/com/aaa/bbb/ccc/languageteacher/di/component/PhraseTranslateActivityComponent.kt
+++ app/src/main/java/com/aaa/bbb/ccc/languageteacher/di/component/PhraseTranslateActivityComponent.kt
@@ -2,11 +2,9 @@ package com.aaa.bbb.ccc.languageteacher.di.component
 
 import com.aaa.bbb.ccc.languageteacher.di.module.PhraseTranslateModule
 import com.aaa.bbb.ccc.languageteacher.di.scope.PhraseTranslatorScope
-import com.aaa.bbb.ccc.languageteacher.presentation.phraseTranslatorActivity.PhraseTranslatorPresenter
 import dagger.Subcomponent
 
 @PhraseTranslatorScope
 @Subcomponent(modules = [PhraseTranslateModule::class])
 interface PhraseTranslateActivityComponent {
-    fun getPresenter(): PhraseTranslatorPresenter
 }
