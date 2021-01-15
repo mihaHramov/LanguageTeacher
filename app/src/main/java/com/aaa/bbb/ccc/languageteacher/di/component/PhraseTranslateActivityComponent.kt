74c8ec2f85c31b36f2bd6b314f3588e51bfda43a
Миша <mihail.hramov.1992@gmail.com>
2021-01-16 01:15:59 +0300

    create method inject

--- app/src/main/java/com/aaa/bbb/ccc/languageteacher/di/component/PhraseTranslateActivityComponent.kt
+++ app/src/main/java/com/aaa/bbb/ccc/languageteacher/di/component/PhraseTranslateActivityComponent.kt
@@ -2,9 +2,11 @@ package com.aaa.bbb.ccc.languageteacher.di.component
 
 import com.aaa.bbb.ccc.languageteacher.di.module.PhraseTranslateModule
 import com.aaa.bbb.ccc.languageteacher.di.scope.PhraseTranslatorScope
+import com.aaa.bbb.ccc.languageteacher.presentation.phraseTranslatorActivity.PhraseTranslatorActivity
 import dagger.Subcomponent
 
 @PhraseTranslatorScope
 @Subcomponent(modules = [PhraseTranslateModule::class])
 interface PhraseTranslateActivityComponent {
+    fun inject(activity: PhraseTranslatorActivity)
 }
